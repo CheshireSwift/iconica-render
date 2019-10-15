a498ab694dd29b17736eb1f01ad5592994c4b894
Sam Collard <gitlab@swifteagle.co.uk>
2019-10-16 00:10:01 +0100

    Card outline styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,97 @@
 import React from 'react'
+import { CSSProperties } from '@emotion/serialize'
 
-const IconicaCard = ({ data }: { data: IconicaCardData }) => <div></div>
+const CardData = React.createContext<IconicaCardData>(
+  (null as unknown) as IconicaCardData,
+)
+
+const IconicaCard = ({
+  data,
+  width,
+}: {
+  data: IconicaCardData
+  width: CSSProperties['width']
+}) => (
+  <CardData.Provider value={data}>
+    <div
+      css={{
+        width,
+        height: `calc(7/5 * ${width})`,
+        background: 'white',
+        borderRadius: `1%`,
+        padding: `calc(${width}/40)`,
+      }}
+    >
+      <div
+        css={{
+          height: '100%',
+          width: '100%',
+          background: '#1B0800',
+          color: 'white',
+          borderRadius: `calc(${width}/40)`,
+        }}
+      >
+        <div
+          css={{
+            display: 'inline-block',
+            height: '100%',
+            width: '83%',
+            background: 'white',
+            borderRadius: `calc(${width}/40)`,
+            paddingRight: '1%',
+          }}
+        >
+          <div
+            css={{
+              height: '100%',
+              width: '100%',
+              background: '#1B0800',
+              borderRadius: `calc(${width}/40)`,
+            }}
+          ></div>
+        </div>
+        <div
+          css={{
+            display: 'inline-block',
+            height: '98.5%',
+            width: '14%',
+            margin: '1%',
+          }}
+        >
+          <div
+            css={{
+              height: '4%',
+              width: '100%',
+              background: 'magenta',
+            }}
+          ></div>
+          <div
+            css={{
+              height: '93%',
+              width: '100%',
+              background: 'white',
+            }}
+          ></div>
+          <div
+            css={{
+              height: '3%',
+              width: '100%',
+              margin: '10%',
+              verticalAlign: 'middle',
+            }}
+          >
+            <strong>Reaction:</strong> 2/40
+          </div>
+        </div>
+      </div>
+    </div>
+  </CardData.Provider>
+)
 
 export const App = () => (
   <div>
     <IconicaCard
+      width="40rem"
       data={{
         faction: 'Independent',
         archetype: 'Fairewood Ring Master',
